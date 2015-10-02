cede2100380f8712656343b60c9049cf2870e412
Sergii Moroz <sergii@comergence.biz>
2015-10-03 01:23:01 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 1;
+        int counter = 0;
+
+
+        do {
+            a = a*2;
+            System.out.println(a);
+            counter++;
+        } while (counter<10);
+    }
+}
